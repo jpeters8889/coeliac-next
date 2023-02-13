d788bee630027900539b44d2bbef1bd60281d1b5
Jamie Peters <jamie@jamie-peters.co.uk>
2023-02-13 18:14:11 +0000

    Add retrospective test to homepage for blogs

--- app/Modules/Shared/Routes/web.php
+++ app/Modules/Shared/Routes/web.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 use App\Modules\Shared\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeController::class);
+Route::get('/', HomeController::class)->name('home');
